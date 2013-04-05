172d40374dab215ad17f13469ad51f632c1d4694
writecoffee <writecoffee@gmail.com>
2013-04-04 20:52:55 -0400

    algo3: hw7: optimization: gradientDescent fixed

--- algo3/hw7/optimization/gradientDescent.m
+++ algo3/hw7/optimization/gradientDescent.m
@@ -1,8 +1,8 @@
 function x=gradientDescent(func, x, scale, eps)
 g = (func(x + eps) - func(x)) / eps;
 update = x - scale * g;
-if abs(x - update) < eps
+if abs(func(x + eps) - func(x)) < eps
     x = x;
 else
-    x = gradientDescent(func, x - scale * g, scale, eps)
+    x = gradientDescent(func, update, scale, eps);
 end
