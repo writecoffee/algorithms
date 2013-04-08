78e17828f28b331025f436032028ef9e6d05c8b3
writecoffee <writecoffee@gmail.com>
2013-04-08 04:01:06 -0400

    algo3: hw7: bridgeRunner got stuck at greedy algo

--- algo3/hw7/stencil/bridgeRunner.m
+++ algo3/hw7/stencil/bridgeRunner.m
@@ -1,1 +1,20 @@
 % Empty file! Put whatever you want here
+function out=bridgeRunner(sz)
+output = zeros(1, sz);
+input = zeros(1, sz);
+input(sz) = 0.2;
+output(sz) = 0.2;
+
+for i = 1 : sz - 1
+    arrayB = fliplr([1:i]);
+    output(sz - i) = sum(output(sz-i+1:sz) .* arrayB ./ 300);
+    output(sz - i) = ceil(output(sz - i) * 100) / 100;
+
+    if output(sz - i) < 0.20
+        output(sz - i) = 0.20;
+    elseif output(sz - i) > 20
+        output(sz - i) = 20;
+    end
+
+end
+out = output;
