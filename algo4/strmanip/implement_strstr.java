0bdf981baf8fa14c04e32dbed90b6ce8ed0a38d7
writecoffee <writecoffee@gmail.com>
2014-01-03 19:39:32 -0500

    algo4: strmanip: implement_strstr: Improved performance using KMP

--- algo4/strmanip/implement_strstr.java
+++ algo4/strmanip/implement_strstr.java
@@ -1,6 +1,6 @@
 public class implement_strstr {
 
-    public static String strStr(String haystack, String needle) {
+    public static String strStrNaive(String haystack, String needle) {
         if (haystack == null) {
             return null;
         }
@@ -33,6 +33,60 @@ public class implement_strstr {
 
         return null;
     }
+    
+    public static String strStr(String haystack, String needle) {
+        int N = haystack.length();
+        int M = needle.length();
+
+        if (M > N) {
+            return null;
+        }
+        if (M == 0) {
+            return haystack;
+        }
+
+        int[] overlay = getOverlay(needle);
+        int i = 0;
+
+comparing:
+        while (i <= N - M) {
+            if (needle.charAt(0) != haystack.charAt(i)) {
+                i++;
+                continue;
+            }
+
+            for (int j = 0; j < M; j++) {
+                if (needle.charAt(j) != haystack.charAt(i + j)) {
+                    i = i + j - overlay[j - 1];
+                    continue comparing;
+                }
+            }
+
+            return haystack.substring(i);
+        }
+
+        return null;
+    }
+
+    public static int[] getOverlay(String needle) {
+        int[] overlay = new int[needle.length()];
+        overlay[0] = 0;
+
+        for (int i = 1; i < overlay.length; i++) {
+            int h = overlay[i - 1];
+            while (h > 0 && needle.charAt(h) != needle.charAt(i)) {
+                h = overlay[h - 1];
+            }
+
+            if (needle.charAt(h) == needle.charAt(i)) {
+                overlay[i] = overlay[i - 1] + 1;
+            } else {
+                overlay[i] = 0;
+            }
+        }
+        
+        return overlay;
+    }
 
     public static void main(String[] args) {
         System.out.println(strStr("hello", ""));
@@ -40,6 +94,6 @@ public class implement_strstr {
         System.out.println(strStr("hello world", "wor"));
         System.out.println(strStr("hello world", "woo"));
         System.out.println(strStr("aaa", "aaaa"));
-        System.out.println(strStr("mississippi", "issipi"));
+        System.out.println(strStr("mississipi", "issipi"));
     }
 }
