f65fe4d81712372a5936dc3fd85ca6586fdb208c
writecoffee <writecoffee@gmail.com>
2014-01-10 10:38:25 -0500

    algo4: miscellaneous: Permutation sequence

---
+++ algo4/miscellaneous/permutation_sequence.java
@@ -0,0 +1,30 @@
+
+public class permutation_sequence {
+
+	static String getPermutation(int n, int k) {
+		StringBuilder numberCollection = new StringBuilder();
+		StringBuilder result = new StringBuilder();
+
+		int factorialProduct = 1;
+		for (int i = 1; i < n + 1; i++) {
+			numberCollection.append((char) ('0' + i));
+			factorialProduct *= i;
+		}
+
+		k--;
+		while (n > 0) {
+			factorialProduct /= n;
+			int numberIndex = k / factorialProduct;
+			k %= factorialProduct;
+			result.append(numberCollection.charAt(numberIndex));
+			numberCollection.delete(numberIndex, numberIndex + 1);
+			n--;
+		}
+
+		return result.toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(getPermutation(3, 3));
+	}
+}
