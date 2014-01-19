c2f15076b2aa4077918f73ca713419b765356fce
writecoffee <writecoffee@gmail.com>
2014-01-19 00:23:46 -0500

    algo4: dp: Unique paths II

---
+++ algo4/dp/unique_paths_II.java
@@ -0,0 +1,27 @@
+public class unique_paths_II {
+    int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+
+        dp[0][0] = 1;
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = obstacleGrid[i][0] == 1 ? 0 : dp[i - 1][0];
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = obstacleGrid[0][j] == 1 ? 0 : dp[0][j - 1];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = obstacleGrid[i][j] == 1 ? 0 : dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
