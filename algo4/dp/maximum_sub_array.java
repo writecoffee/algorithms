cdf99c7f1331cfa3c880657948d71abd406df8d3
writecoffee <writecoffee@gmail.com>
2014-01-18 21:52:32 -0500

    algo4: dp: Maximum sub array

---
+++ algo4/dp/maximum_sub_array.java
@@ -0,0 +1,41 @@
+public class maximum_sub_array {
+
+    public static int maxSubArray(int[] A) {
+        int globalMaxSum = A[0];
+        int localMaxSum = A[0];
+        for (int i = 1; i < A.length; i++) {
+            localMaxSum = Math.max(A[i], localMaxSum + A[i]);
+            globalMaxSum = Math.max(localMaxSum, globalMaxSum);
+        }
+
+        return globalMaxSum;
+    }
+
+    public static int[] maxSubArrayWithIndices(int[] A) {
+        int globalMaxSum = A[0];
+        int localMaxSum = A[0];
+        int start = 0;
+        int end = 0;
+
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] > localMaxSum + A[i]) {
+                localMaxSum = A[i];
+                start = i;
+            } else {
+                localMaxSum = localMaxSum + A[i];
+            }
+
+            if (localMaxSum > globalMaxSum) {
+                globalMaxSum = localMaxSum;
+                end = i;
+            }
+        }
+
+        return new int[] { globalMaxSum, start, end };
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+        maxSubArrayWithIndices(new int[] { -1, -1, -1, 4, -1, 2, 1, -5, 4 });
+    }
+}
