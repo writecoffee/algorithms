29ae040ff84864aaef595b48af575aabe863404f
writecoffee <writecoffee@gmail.com>
2014-01-20 22:25:15 -0500

    algo4: dfs: generate_parentheses: Iterative backtracking method

--- algo4/dfs/generate_parentheses.java
+++ algo4/dfs/generate_parentheses.java
@@ -31,7 +31,79 @@ public class generate_parentheses {
         }
     }
 
+    public static ArrayList<String> generateParenthesisImprov(int n) {
+        ArrayList<String> result = new ArrayList<String>();
+        if (n == 0) {
+            return result;
+        }
+
+        int[] stack = new int[2 * n];
+        int stackPointer = 0;
+        int cl = n;
+        int cr = n;
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            if (stackPointer == 2 * n) {
+                result.add(sb.toString());
+                stackPointer--;
+            } else if (stack[stackPointer] > 2) {
+                if (stackPointer == 0) {
+                    break;
+                }
+
+                assert sb.length() == stackPointer + 1;
+
+                stack[stackPointer] = 0;
+
+                if (sb.charAt(stackPointer) == ')') {
+                    cr++;
+                } else {
+                    cl++;
+                }
+
+                sb.delete(sb.length() - 1, sb.length());
+                stackPointer--;
+            } else {
+                stack[stackPointer]++;
+                if (stack[stackPointer] == 1) {
+                    if (cl == 0) {
+                        continue;
+                    } else {
+                        sb.append('(');
+                        cl--;
+                    }
+                } else if (stack[stackPointer] == 2) {
+                    boolean backtracked = false;
+                    if (sb.length() - 1 == stackPointer) {
+                        cl++;
+                        sb.delete(sb.length() - 1, sb.length());
+                        backtracked = true;
+                    }
+
+                    if (cl < cr) {
+                        sb.append(')');
+                        cr--;
+                    } else {
+                        stack[stackPointer]++;
+                        if (backtracked) {
+                            sb.append('(');
+                            cl--;
+                        }
+                        continue;
+                    }
+                } else {
+                    continue;
+                }
+
+                stackPointer++;
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         generateParenthesis(3);
+        generateParenthesisImprov(3);
     }
 }
