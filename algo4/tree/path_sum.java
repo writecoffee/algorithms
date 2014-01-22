f73eb648a5849c39750d4d19e54b1ed5cc749b4b
writecoffee <writecoffee@gmail.com>
2014-01-22 02:40:26 -0500

    algo4: tree: Path sum

---
+++ algo4/tree/path_sum.java
@@ -0,0 +1,24 @@
+public class path_sum {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            return sum == root.val;
+        }
+
+        return hasPathSum(root.left, sum - root.val) || 
+               hasPathSum(root.right, sum - root.val);
+    }
+}
