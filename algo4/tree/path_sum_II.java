8c926b67ac72d7aab998b978eaaa53a075ddf183
writecoffee <writecoffee@gmail.com>
2014-01-22 19:38:07 -0500

    algo4: tree: Path sum II nonrecursive solution using queue

--- algo4/tree/path_sum_II.java
+++ algo4/tree/path_sum_II.java
@@ -1,4 +1,6 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 
 public class path_sum_II {
     public class TreeNode {
@@ -37,7 +39,61 @@ public class path_sum_II {
 
         probe(root.left, sum - root.val, result, path);
         probe(root.right, sum - root.val, result, path);
-        
+
         path.remove(path.size() - 1);
+    }
+
+    public static class MyTreeNode {
+        final TreeNode treeNode;
+        final ArrayList<Integer> pathSoFar;
+        final int sumSoFar;
+        MyTreeNode(TreeNode treeNode, ArrayList<Integer> pathSoFar, int sumSoFar) {
+            this.pathSoFar = pathSoFar;
+            this.treeNode = treeNode;
+            this.sumSoFar = sumSoFar;
+        }
+    }
+
+    public ArrayList<ArrayList<Integer>> pathSumNonrecur(TreeNode root, int sum) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Deque<MyTreeNode> intermediate = new ArrayDeque<MyTreeNode>();
+        Deque<MyTreeNode> innerIntermediate = new ArrayDeque<MyTreeNode>();
+        intermediate.addLast(new MyTreeNode(root, new ArrayList<Integer>(), 0));
+        while (!intermediate.isEmpty()) {
+            Deque<MyTreeNode> temp = intermediate;
+            intermediate = innerIntermediate;
+            innerIntermediate = temp;
+
+            while (!innerIntermediate.isEmpty()) {
+                MyTreeNode current = innerIntermediate.pollFirst();
+                if (current.treeNode.left == null &&
+                    current.treeNode.right == null &&
+                    sum - current.sumSoFar - current.treeNode.val == 0) {
+                    ArrayList<Integer> newPath = new ArrayList<Integer>(current.pathSoFar);
+                    newPath.add(current.treeNode.val);
+                    result.add(newPath);
+                    continue;
+                }
+
+                if (current.treeNode.left != null) {
+                    ArrayList<Integer> newPath = new ArrayList<Integer>(current.pathSoFar);
+                    newPath.add(current.treeNode.val);
+                    intermediate.addLast(new MyTreeNode(current.treeNode.left, newPath, current.sumSoFar + current.treeNode.val));
+                }
+
+                if (current.treeNode.right != null) {
+                    ArrayList<Integer> newPath = new ArrayList<Integer>(current.pathSoFar);
+                    newPath.add(current.treeNode.val);
+                    intermediate.addLast(new MyTreeNode(current.treeNode.right, newPath, current.sumSoFar + current.treeNode.val));
+                }
+            }
+        }
+
+        return result;
     }
 }
