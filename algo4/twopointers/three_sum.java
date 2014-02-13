09707d2ba2ce1cdec6a95416bd7a1d8a528ee2a0
writecoffee <writecoffee@gmail.com>
2014-02-13 17:05:58 -0500

    algo4: twopointers: Four sum     -- twopointers: three_sum: Sort array with Arrays.sort()

--- algo4/twopointers/three_sum.java
+++ algo4/twopointers/three_sum.java
@@ -1,57 +1,51 @@
-
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class three_sum {
     static ArrayList<ArrayList<Integer>> threeSum(int[] num) {
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> sortedNums = new ArrayList<Integer>();
-        for (int i = 0; i < num.length; i++) {
-            sortedNums.add(num[i]);
-        }
-        Collections.sort(sortedNums);
+        Arrays.sort(num);
 
         int N = num.length;
-        for (int i = 0; i < N - 2 && sortedNums.get(i) <= 0; i++) {
-            if (i > 0 && sortedNums.get(i) == sortedNums.get(i - 1)) {
+        for (int i = 0; i < N - 2 && num[i] <= 0; i++) {
+            if (i > 0 && num[i] == num[i - 1]) {
                 continue;
             }
 
-            int expectedTwoSum = 0 - sortedNums.get(i);
+            int expectedTwoSum = 0 - num[i];
             int l = i + 1;
             int r = N - 1;
             while (l < r) {
-                int sum = sortedNums.get(l) + sortedNums.get(r);
+                int sum = num[l] + num[r];
                 if (sum < expectedTwoSum) {
                     ++l;
                 } else if (sum > expectedTwoSum) {
                     --r;
                 } else {
                     ArrayList<Integer> triple = new ArrayList<Integer>();
-                    triple.add(sortedNums.get(i));
-                    triple.add(sortedNums.get(l));
-                    triple.add(sortedNums.get(r));
+                    triple.add(num[i]);
+                    triple.add(num[l]);
+                    triple.add(num[r]);
                     result.add(triple);
-                    
+
                     l++;
                     r--;
-                    while (l < r && sortedNums.get(l) == sortedNums.get(l - 1)) {
+                    while (l < r && num[l] == num[l - 1]) {
                         l++;
                     }
-                    while (l < r && sortedNums.get(r) == sortedNums.get(r + 1)) {
+                    while (l < r && num[r] == num[r + 1]) {
                         r--;
                     }
                 }
             }
-            
+
         }
-        
+
         return result;
     }
 
     public static void main(String[] args) {
-        threeSum(new int[] {-1, 0, 1, 2, -1, -4});
+        threeSum(new int[] { -1, 0, 1, 2, -1, -4 });
         threeSum(new int[] { -2, 0, 1, 1, 2 });
-
     }
 }
