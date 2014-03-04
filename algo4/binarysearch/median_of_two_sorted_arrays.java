326bbd01a4e167d634f22ee03cc2522c42a651cb
writecoffee <writecoffee@gmail.com>
2014-03-04 03:34:41 -0500

    algo4: binarysearch: median_of_two_sorted_arrays: Revisited     -- let middle pointer to be length / 2, recite it for now

--- algo4/binarysearch/median_of_two_sorted_arrays.java
+++ algo4/binarysearch/median_of_two_sorted_arrays.java
@@ -1,65 +1,51 @@
-import java.util.List;
-import java.util.ArrayList;
-
 public class median_of_two_sorted_arrays {
-
     static double findMedianSortedArrays(int[] A, int[] B) {
-        int totalLength = A.length + B.length;
+        int m = A.length;
+        int n = B.length;
 
-        ArrayList<Integer> arrayA = new ArrayList<Integer>();
-        ArrayList<Integer> arrayB = new ArrayList<Integer>();
-
-        for (int i = 0; i < A.length; i++) {
-            arrayA.add(A[i]);
-        }
-
-        for (int i = 0; i < B.length; i++) {
-            arrayB.add(B[i]);
-        }
-
-        if ((totalLength & 0x1) > 0) {
-            return findKthInSortedArrays(arrayA, arrayB, totalLength / 2 + 1);
+        if ((m + n) % 2 == 1) {
+            return findMedianSortedArraysSub(A, 0, m, B, 0, n, (m + n - 1) / 2);
         } else {
-            return (findKthInSortedArrays(arrayA, arrayB, totalLength / 2) +
-                    findKthInSortedArrays(arrayA, arrayB, totalLength / 2 + 1)) / 2;
+            return (findMedianSortedArraysSub(A, 0, m, B, 0, n, (m + n - 1) / 2)
+                  + findMedianSortedArraysSub(A, 0, m, B, 0, n, (m + n) / 2)) / 2;
         }
     }
 
-    private static double findKthInSortedArrays(List<Integer> A, List<Integer> B, int k) {
-        int n = A.size();
-        int m = B.size();
-
-        if (n == 0) {
-            return B.get(k - 1);
-        }
+    private static double findMedianSortedArraysSub(int[] A, int aStart, int aEnd, int[] B, int bStart, int bEnd, int k) {
+        int m = aEnd - aStart;
+        int n = bEnd - bStart;
+        int aMid = aStart + m / 2;
+        int bMid = bStart + n / 2;
 
         if (m == 0) {
-            return A.get(k - 1);
+            return B[k];
+        } else if (n == 0) {
+            return A[k];
         }
 
-        if (k == 1) {
-            return Math.min(A.get(0), B.get(0));
+        if (k == 0) {
+            return A[0] > B[0] ? B[0] : A[0];
         }
 
-        if (B.get(m / 2) >= A.get(n / 2)) {
-            if ((n / 2 + 1 + m / 2) >= k) {
-                return findKthInSortedArrays(A, B.subList(0, m / 2), k);
+        if (B[bMid] >= A[aMid]) {
+            if (k <= (n / 2 + m / 2)) {
+                return findMedianSortedArraysSub(A, aStart, aEnd, B, bStart, bMid, k);
             } else {
-                return findKthInSortedArrays(A.subList(n / 2 + 1, n), B, k - (n / 2 + 1));
+                return findMedianSortedArraysSub(A, aMid + 1, aEnd, B, bStart, bEnd, k - (m / 2 + 1));
             }
         } else {
-            if ((m /2 + 1 + n / 2) >= k) {
-                return findKthInSortedArrays(A.subList(0, n / 2), B, k);
+            if (k <= (n / 2 + m / 2)) {
+                return findMedianSortedArraysSub(A, aStart, aMid, B, bStart, bEnd, k);
             } else {
-                return findKthInSortedArrays(A, B.subList(m / 2 + 1, m), k - (m / 2 + 1));
+                return findMedianSortedArraysSub(A, aStart, aEnd, B, bMid + 1, bEnd, k - (n / 2 + 1));
             }
         }
     }
 
     public static void main(String[] args) {
         System.out.println(findMedianSortedArrays(new int[] { 100000 }, new int[] { 100001 }));
-        System.out.println(findMedianSortedArrays(new int[] { 1, 2 }, new int[] { 1, 2 }));
         System.out.println(findMedianSortedArrays(new int[] { 3, 4 }, new int[] { 1, 2 }));
+        System.out.println(findMedianSortedArrays(new int[] { 1, 2 }, new int[] { 1, 2 }));
         System.out.println(findMedianSortedArrays(new int[] { 1, 5, 6, 7 }, new int[] { 2, 3, 4, 8 }));
         System.out.println(findMedianSortedArrays(new int[] { 1, 2, 3, 4 }, new int[] { 5, 6, 7, 8, 9, 10 }));
         System.out.println(findMedianSortedArrays(new int[] { 4 }, new int[] { 1, 2, 3 }));
