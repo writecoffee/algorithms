3e8f7a4b87342228c846ccc620a7d81e775169fd
writecoffee <writecoffee@gmail.com>
2014-03-06 17:54:50 -0500

    algo4: dp: scramble_string: Revisited -- 3-dimension dp

--- algo4/dp/scramble_string.java
+++ algo4/dp/scramble_string.java
@@ -1,4 +1,3 @@
-
 public class scramble_string {
     public static boolean isScramble(String s1, String s2) {
         int len = s1.length();
@@ -7,20 +6,19 @@ public class scramble_string {
             return false;
         }
 
-        if (s1.equals(s2)) {
-            return true;
+        boolean[][][] dp = new boolean[len + 1][len][len];
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                dp[1][i][j] = s1.charAt(i) == s2.charAt(j);
+            }
         }
 
-        boolean[][][] dp = new boolean[len + 1][len][len];
-        for (int k = 1; k <= len; k++) {
-            for (int i = 0; i <= len - k; i++) {
-                for (int j = 0; j <= len - k; j++) {
-                    if (k == 1) {
-                        dp[1][i][j] = (s1.charAt(i) == s2.charAt(j));
-                    }
-
-                    for (int p = 1; p < k && !dp[k][i][j]; p++) {
-                        if (dp[p][i][j] && dp[k - p][i + p][j + p] || dp[p][i][j + k - p] && dp[k - p][i + p][j]) {
+        for (int k = 2; k < len + 1; k++) {
+            for (int i = 0; i < len - k + 1; i++) {
+                for (int j = 0; j < len - k + 1; j++) {
+                    for (int p = 1; p < k; p++) {
+                        if ((dp[p][i][j] && dp[k - p][i + p][j + p])
+                         || (dp[p][i][j + k - p] && dp[k - p][i + p][j])) {
                             dp[k][i][j] = true;
                         }
                     }
@@ -32,6 +30,8 @@ public class scramble_string {
     }
 
     public static void main(String[] args) {
-        isScramble("great", "rgtae");
+        assert isScramble("great", "rgtae");
+        assert isScramble("a", "a");
+        assert isScramble("ba", "ab");
     }
 }
