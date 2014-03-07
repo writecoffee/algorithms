414688d6cd9a1d270a5e233596dd1268b7ca4fc3
writecoffee <writecoffee@gmail.com>
2014-03-06 22:03:51 -0500

    algo4: combination: Gray code     -- created combination/ category

---
+++ algo4/combination/gray_code.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class gray_code {
+    public ArrayList<Integer> grayCode(int n) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+
+        result.add(0);
+        for (int i = 0; i < n; i++) {
+            int flipMask = 1 << i;
+
+            for (int j = result.size() - 1; j >= 0; j--) {
+                result.add(result.get(j) | flipMask);
+            }
+        }
+
+        return result;
+    }
+}
