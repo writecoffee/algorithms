42ae6843f469fd4450017748af6de4e983135e35
writecoffee <writecoffee@gmail.com>
2014-03-25 17:22:37 -0400

    algo4: bitmanip: Single number

---
+++ algo4/bitmanip/single_number.java
@@ -0,0 +1,9 @@
+public class single_number {
+    public int singleNumber(int[] A) {
+        int result = 0;
+        for (int i = 0; i < A.length; ++i) {
+            result ^= A[i];
+        }
+        return result;
+    }
+}
