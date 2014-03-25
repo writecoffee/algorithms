e4771e27c48bf3f796082da27cb0fe49f5136bc0
writecoffee <writecoffee@gmail.com>
2014-03-25 02:51:36 -0400

    algo4: bitmanip: Single number II

---
+++ algo4/bitmanip/single_number_II.java
@@ -0,0 +1,23 @@
+public class single_number_II {
+    int singleNumber(int A[]) {
+        int result = 0;
+        int n = A.length;
+
+        for (int i = 0; i < 32; ++i) {
+            int count = 0;
+            int bit = 1 << i;
+
+            for (int j = 0; j < n; ++j) {
+                if ((A[j] & bit) != 0) {
+                    count++;
+                }
+            }
+
+            if (count % 3 != 0) {
+                result |= bit;
+            }
+        }
+
+        return result;
+    }
+}
