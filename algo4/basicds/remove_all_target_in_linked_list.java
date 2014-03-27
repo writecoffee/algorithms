4f6f5e0eebeb35b287134da939afc5703df24884
writecoffee <writecoffee@gmail.com>
2014-03-26 23:03:51 -0400

    algo4: basicds: Remove all targets in linked list

---
+++ algo4/basicds/remove_all_target_in_linked_list.java
@@ -0,0 +1,22 @@
+public class remove_all_target_in_linked_list {
+    static class LinkedListNode {
+        int val;
+        LinkedListNode next;
+    }
+
+    static LinkedListNode removeAll(int val, LinkedListNode list) {
+        LinkedListNode psudoHead = new LinkedListNode();
+        psudoHead.next = list;
+
+        LinkedListNode c = psudoHead;
+        while (c.next != null) {
+            if (c.next.val == val) {
+                c.next = c.next.next;
+            } else {
+                c = c.next;
+            }
+        }
+
+        return psudoHead.next;
+    }
+}
