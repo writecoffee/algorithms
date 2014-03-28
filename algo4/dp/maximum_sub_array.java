57def9e00085191cd12edb88c1e533c76131ee51
writecoffee <writecoffee@gmail.com>
2014-03-28 02:06:52 -0400

    algo4: dp: maximum_sub_array: Revisited and added variance -- circle and non-negative max

--- algo4/dp/maximum_sub_array.java
+++ algo4/dp/maximum_sub_array.java
@@ -61,8 +61,9 @@ public class maximum_sub_array {
 
     /**
      * A variance of this problem: if the maximum sum is negative, return 0. 
+     * 
      */
-    public int maxConsSum(int[] arr) {
+    public static int maxConsSum(int[] arr) {
         int n = arr.length;
         if (n == 0) {
             return 0;
@@ -85,11 +86,53 @@ public class maximum_sub_array {
         return gMax;
     }
 
+    /**
+     * A variance of this problem: if the array represents a circle.
+     * This solution uses two arrays to store maximum sum for arr[0 .. i]
+     * and arr[i .. n - 1].
+     */
+    public static int maxConsSumInCircle(int[] arr) {
+        int n = arr.length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int nMax = maxConsSum(arr);
+
+        int[] l2r = new int[n];
+        int[] r2l = new int[n];
+
+        l2r[0] = Math.max(0, arr[0]);
+        int sum = arr[0];
+        for (int i = 1; i < n; ++i) {
+            sum += arr[i];
+            l2r[i] = Math.max(l2r[i - 1], sum);
+        }
+
+        r2l[n - 1] = Math.max(0, arr[n - 1]);
+        sum = arr[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            sum += arr[i];
+            r2l[i] = Math.max(r2l[i + 1], sum);
+        }
+
+        for (int i = 0; i < n - 1; ++i) {
+            nMax = Math.max(nMax, l2r[i] + r2l[i + 1]);
+        }
+
+        return nMax;
+    }
+
     public static void main(String[] args) {
         System.out.println("Max Sum: " + maxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
         System.out.println("Sum Len: " + maxSubArrayLength(new int[] { -1, -1, -1, 4, -1, 2, 1, -5, 4 }));
 
         maxSubArrayWithIndices(new int[] { -1, -1, -1, 4, -1, 2, 1, -5, 4 });
         maxSubArrayWithIndices(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
+
+        System.out.println(maxConsSumInCircle(new int[] { 1, 2, -3, 4, -1, 0, 2 }));
+        System.out.println(maxConsSumInCircle(new int[] { 1, -2, 100, -1, 2, -3 }));
+        
+
     }
 }
