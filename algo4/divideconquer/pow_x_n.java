0e37fc51ab850df662718b291c1d9a42ddb61f45
writecoffee <writecoffee@gmail.com>
2014-03-30 20:39:04 -0400

    algo4: divideconquer: pow_x_n: Revisited

--- algo4/divideconquer/pow_x_n.java
+++ algo4/divideconquer/pow_x_n.java
@@ -1,4 +1,3 @@
-
 public class pow_x_n {
     public static double pow(double x, int n) {
         if (n == 0) {
@@ -13,19 +12,16 @@ public class pow_x_n {
         if (n == 1) {
             return x;
         }
-        
+
         if (n % 2 == 0) {
             return pow(pow(x, n / 2), 2);
         } else {
-            if (n > 0) {
-                return x * pow(pow(x, n / 2), 2);
-            } else {
-                return 1 / x * pow(pow(x, n / 2), 2);
-            }
+            return (n > 0 ? x : 1 / x) * pow(pow(x, n / 2), 2);
         }
     }
 
     public static void main(String[] args) {
         System.out.println(pow(99.4f, 2));
+        System.out.println(pow(34.00515f, -3));
     }
 }
