f8b5c24dfa4bdef5919bd1cd3c3fa699e88f3e78
writecoffee <writecoffee@gmail.com>
2014-04-06 01:00:49 -0400

    algo4: tree: bst: validate_bst: Non-recur solution for tree with duplicates

--- algo4/tree/bst/validate_bst.java
+++ algo4/tree/bst/validate_bst.java
@@ -1,5 +1,6 @@
 package bst;
 
+import java.util.HashMap;
 import java.util.Stack;
 
 public class validate_bst {
@@ -15,12 +16,14 @@ public class validate_bst {
         }
     }
 
+    /**
+     * Only work when the tree contains no duplicate values. 
+     */
     public boolean isValidBST(TreeNode root) {
         return explore(root);
     }
 
     TreeNode pre = null;
-
     public boolean explore(TreeNode root) {
         if (root == null) {
             return true;
@@ -38,6 +41,9 @@ public class validate_bst {
         return explore(root.right);
     }
 
+    /**
+     * Work regardless of whether the tree has duplicate values or not. 
+     */
     boolean checkBSTWithMaxMinRange(TreeNode root, int min, int max) {
         if (root == null) {
             return true;
@@ -51,7 +57,11 @@ public class validate_bst {
             && checkBSTWithMaxMinRange(root.right, root.val, max);
     }
 
-    public boolean isValidBSTNonrecur(TreeNode root) {
+    /**
+     * For any node T, T's value should be > any node in its left sub-tree
+     * and < any node in its right sub-tree.
+     */
+    public boolean isValidBSTNonrecurNodup(TreeNode root) {
         Stack<TreeNode> s = new Stack<TreeNode>();
         TreeNode next = root;
         TreeNode last = null;
@@ -69,6 +79,53 @@ public class validate_bst {
 
                 last = c;
                 next = c.right;
+            }
+        }
+
+        return true;
+    }
+
+    class MaxMinPair {
+        final int max;
+        final int min;
+        MaxMinPair(int _min, int _max) {
+            min = _min;
+            max = _max;
+        }
+    }
+
+    /**
+     * For any node T, T's value should be >= any node in its left sub-tree
+     * and < any node in its right sub-tree.
+     */
+    public boolean isValidBSTNonrecur(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        s.push(root);
+        HashMap<TreeNode, MaxMinPair> h = new HashMap<TreeNode, MaxMinPair>();
+        h.put(root, new MaxMinPair(Integer.MIN_VALUE, Integer.MAX_VALUE));
+
+        while (!s.isEmpty()) {
+            TreeNode c = s.pop();
+            MaxMinPair info = h.get(c);
+
+            if (info.min >= c.val || info.max < c.val) {
+                return false;
+            } else {
+                h.remove(c);
+            }
+
+            if (c.right != null) {
+                s.push(c.right);
+                h.put(c.right, new MaxMinPair(c.val, info.max));
+            }
+
+            if (c.left != null) {
+                s.push(c.left);
+                h.put(c.left, new MaxMinPair(info.min, c.val));
             }
         }
 
