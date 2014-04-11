50ace8a5c3024d216d82bc4a0a70f9015de6dd73
writecoffee <writecoffee@gmail.com>
2014-04-10 23:32:46 -0400

    algo4: linkedlist: Merge k sorted linked list

--- algo4/linkedlist/merge_k_sorted_linked_list.java
+++ algo4/linkedlist/merge_k_sorted_linked_list.java
@@ -14,55 +14,29 @@ public class merge_k_sorted_linked_list {
     }
 
     public static ListNode mergeKLists(ArrayList<ListNode> lists) {
-        Comparator<ListNode> comp = new Comparator<ListNode>() {
+        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(lists.size(), new Comparator<ListNode>() {
             @Override
-            public int compare(ListNode o1, ListNode o2) {
-                return o1.val - o2.val;
+            public int compare(ListNode a, ListNode b) {
+                return a.val - b.val;
             }
-        };
+        });
 
-        PriorityQueue<ListNode> q = new PriorityQueue<ListNode>(lists.size(), comp);
         for (int i = 0; i < lists.size(); i++) {
-            q.add(lists.get(i));
+            pq.add(lists.get(i));
         }
 
-        ListNode psudoHead = new ListNode(-1);
-        ListNode curr = psudoHead;
-        while (!q.isEmpty()) {
-            ListNode minNode = q.poll();
+        ListNode psudoHead = new ListNode(-1), h = psudoHead;
+        while (!pq.isEmpty()) {
+            ListNode c = pq.poll();
 
-            curr.next = minNode;
-            curr = curr.next;
+            h.next = c;
+            h = h.next;
 
-            if (minNode.next != null) {
-                q.add(minNode.next);
+            if (c.next != null) {
+                pq.add(c.next);
             }
         }
 
         return psudoHead.next;
-    }
-
-    private static ListNode toLinkedList(int[] numbers) {
-        ListNode psudoHead = new ListNode(-1);
-        for (int i = numbers.length - 1; i >= 0; --i) {
-            ListNode temp = psudoHead.next;
-            psudoHead.next = new ListNode(numbers[i]);
-            psudoHead.next.next = temp;
-        }
-
-        return psudoHead.next;
-    }
-
-    public static void main(String[] args) {
-        ListNode l1 = toLinkedList(new int[] { 1, 2, 2, 3, 4 });
-        ListNode l2 = toLinkedList(new int[] { 1, 2 });
-        ListNode l3 = toLinkedList(new int[] { 6, 9 });
-
-        ArrayList<ListNode> myList = new ArrayList<ListNode>();
-        myList.add(l1);
-        myList.add(l2);
-        myList.add(l3);
-        
-        mergeKLists(myList);
     }
 }
