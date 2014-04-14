fd656f682c07e327d42efbc6397620ed10c7e9e0
writecoffee <writecoffee@gmail.com>
2014-04-14 03:16:09 -0400

    algo4: graph: clone_graph: Revisited and added dfs method

--- algo4/graph/clone_graph.java
+++ algo4/graph/clone_graph.java
@@ -20,32 +20,51 @@ public class clone_graph {
         }
 
         Queue<UndirectedGraphNode> q = new LinkedList<UndirectedGraphNode>();
-        Queue<UndirectedGraphNode> p;
-        HashMap<Integer, UndirectedGraphNode> visited = new HashMap<Integer, UndirectedGraphNode>();
+        HashMap<Integer, UndirectedGraphNode> h = new HashMap<Integer, UndirectedGraphNode>();
         q.add(node);
-        visited.put(node.label, new UndirectedGraphNode(node.label));
+        h.put(node.label, new UndirectedGraphNode(node.label));
 
         while (!q.isEmpty()) {
-            p = new LinkedList<UndirectedGraphNode>(q);
-            q.clear();
+            UndirectedGraphNode v = q.poll();
+            UndirectedGraphNode vCloned = h.get(v.label);
 
-            while (!p.isEmpty()) {
-                UndirectedGraphNode v = p.poll();
-                UndirectedGraphNode vCloned = visited.get(v.label);
-
-                for (UndirectedGraphNode u : v.neighbors) {
-                    if (!visited.containsKey(u.label)) {
-                        UndirectedGraphNode uCloned = new UndirectedGraphNode(u.label);
-                        vCloned.neighbors.add(uCloned);
-                        visited.put(u.label, uCloned);
-                        q.add(u);
-                    } else {
-                        vCloned.neighbors.add(visited.get(u.label));
-                    }
+            for (UndirectedGraphNode u : v.neighbors) {
+                if (!h.containsKey(u.label)) {
+                    UndirectedGraphNode uCloned = new UndirectedGraphNode(u.label);
+                    vCloned.neighbors.add(uCloned);
+                    h.put(u.label, uCloned);
+                    q.add(u);
+                } else {
+                    vCloned.neighbors.add(h.get(u.label));
                 }
             }
         }
 
-        return visited.get(node.label);
+        return h.get(node.label);
+    }
+
+    public UndirectedGraphNode cloneGraphDFS(UndirectedGraphNode node) {
+        if (node == null) {
+            return null;
+        }
+
+        HashMap<Integer, UndirectedGraphNode> h = new HashMap<Integer, UndirectedGraphNode>();
+        UndirectedGraphNode nodeCloned = new UndirectedGraphNode(node.label);
+        h.put(node.label, nodeCloned);
+        explore(node, nodeCloned, h);
+        return nodeCloned;
+    }
+
+    private void explore(UndirectedGraphNode c, UndirectedGraphNode cCloned, HashMap<Integer, UndirectedGraphNode> h) {
+        for (UndirectedGraphNode n : c.neighbors) {
+            if (!h.containsKey(n.label)) {
+                UndirectedGraphNode nCloned = new UndirectedGraphNode(n.label);
+                h.put(n.label, nCloned);
+                cCloned.neighbors.add(nCloned);
+                explore(n, nCloned, h);
+            } else {
+                cCloned.neighbors.add(h.get(n.label));
+            }
+        }
     }
 }
