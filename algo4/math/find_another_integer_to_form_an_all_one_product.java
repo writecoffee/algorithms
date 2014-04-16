5b717600d488bd09f9ee61b125f256c5267563e5
writecoffee <writecoffee@gmail.com>
2014-04-15 20:52:52 -0400

    algo4: math: find_another_integer_to_form_an_all_one_product: Revisited

--- algo4/math/find_another_integer_to_form_an_all_one_product.java
+++ algo4/math/find_another_integer_to_form_an_all_one_product.java
@@ -1,8 +1,10 @@
 public class find_another_integer_to_form_an_all_one_product {
-    static int findMinAllOne(int a) {
+    public int findMinAllOne(int a) {
         int[] m = new int[] { 0, 1, 0, 1, 0, 0, 0, 1, 0, 1 };
         if (m[a % 10] == 0) {
             return -1;
+        } else if (a == 1) {
+            return 1;
         }
 
         int count = 1;
@@ -11,13 +13,6 @@ public class find_another_integer_to_form_an_all_one_product {
             count++;
         }
 
-        return a == 1 ? 1 : count;
-    }
-
-    public static void main(String[] args) {
-        assert findMinAllOne(3) == 3;
-        assert findMinAllOne(33) == 6;
-        assert findMinAllOne(24) == -1;
-        assert findMinAllOne(1) == 1;
+        return count;
     }
 }
