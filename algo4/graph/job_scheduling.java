119e9dc06f26912234ecd73ff12a9695736dd604
writecoffee <writecoffee@gmail.com>
2014-04-15 22:12:44 -0400

    algo4: graph: job_scheduling: Revisited

--- algo4/graph/job_scheduling.java
+++ algo4/graph/job_scheduling.java
@@ -1,38 +1,35 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Queue;
 
 public class job_scheduling {
     public boolean jobSchedule(Map<Integer, List<Integer>> deps, int n, int[] result) {
-        HashMap<Integer, ArrayList<Integer>> notify = new HashMap<Integer, ArrayList<Integer>>();
-        HashMap<Integer, Integer> inDegrees = new HashMap<Integer, Integer>();
+        HashMap<Integer, ArrayList<Integer>> hNotify = new HashMap<Integer, ArrayList<Integer>>();
+        HashMap<Integer, Integer> hInDegrees = new HashMap<Integer, Integer>();
         Queue<Integer> q = new LinkedList<Integer>();
 
         for (int i = 0; i < n; ++i) {
-            inDegrees.put(i + 1, 0);
-            notify.put(i + 1, new ArrayList<Integer>());
+            hNotify.put(i + 1, new ArrayList<Integer>());
+            hInDegrees.put(i + 1, 0);
         }
 
         for (Entry<Integer, List<Integer>> entry : deps.entrySet()) {
             int jobId = entry.getKey();
             List<Integer> depIds = entry.getValue();
 
-            inDegrees.put(jobId, depIds.size());
+            hInDegrees.put(jobId, depIds.size());
             for (Integer depId : depIds) {
-                notify.get(depId).add(jobId);
+                hNotify.get(depId).add(jobId);
             }
         }
 
-        for (Entry<Integer, Integer> entry : inDegrees.entrySet()) {
-            int jobId = entry.getKey();
-            int inDegree = entry.getValue();
-
-            if (inDegree == 0) {
-                q.add(jobId);
+        for (Entry<Integer, Integer> entry : hInDegrees.entrySet()) {
+            if (entry.getValue() == 0) {
+                q.add(entry.getKey());
             }
         }
 
@@ -41,10 +38,9 @@ public class job_scheduling {
             Integer jobId = q.poll();
             result[i++] = jobId;
 
-            for (Integer successor : notify.get(jobId)) {
-                int depCount = inDegrees.put(successor, inDegrees.get(successor) - 1) - 1;
-                if (depCount == 0) {
-                    q.add(successor);
+            for (Integer nxt : hNotify.get(jobId)) {
+                if (hInDegrees.put(nxt, hInDegrees.get(nxt) - 1) == 1) {
+                    q.add(nxt);
                 }
             }
         }
