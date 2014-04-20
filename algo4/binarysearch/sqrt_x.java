aaca97d849c670fa3f8563d46bde438135d5a810
writecoffee <writecoffee@gmail.com>
2014-04-20 00:15:40 -0400

    algo4: binarysearch: sqrt_x: Polished the solution

--- algo4/binarysearch/sqrt_x.java
+++ algo4/binarysearch/sqrt_x.java
@@ -11,22 +11,32 @@ public class sqrt_x {
             return x;
         }
 
-        int low = 0;
-        int high = x;
+        int l = 1;
+        int r = x;
 
-        while (high > low + 1) {
-            int mid = low + (high - low) / 2;
-            int div = x / mid;
+        /**
+         * Here we need the while loop terminates, if a^2 is slightly smaller than x and b^2
+         * is slightly larger than x, we will return a as square root. An apparent test case
+         * for this is let x = 2.
+         */
+        while (r > l + 1) {
+            /**
+             * Here we can come with test x = 2147483647 to let (l + r) overflow.
+             */
+            int a = l + (r - l) / 2;
+            int b = x / a;
 
-            if (mid == div) {
-                return mid;
-            } else if (mid < div) {
-                low = mid;
+            if (a == b) {
+                return a;
+            } else if (a < b) {
+                l = a;
+                r = b;
             } else {
-                high = mid;
+                l = b;
+                r = a;
             }
         }
 
-        return low;
+        return l;
     }
 }
