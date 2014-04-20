3a19a92481239a300979bc2cba3cc746f590930c
writecoffee <writecoffee@gmail.com>
2014-04-20 00:57:26 -0400

    algo4: binarysearch: search_insert_position: Polished

--- algo4/binarysearch/search_insert_position.java
+++ algo4/binarysearch/search_insert_position.java
@@ -1,23 +1,20 @@
 public class search_insert_position {
+    public int searchInsert(int[] array, int target) {
+        int l = 0;
+        int r = array.length - 1;
 
-    public int searchInsert(int[] A, int target) {
-        int low = 0;
-        int high = A.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (A[mid] == target) {
+            if (array[mid] == target) {
                 return mid;
-            }
-
-            if (A[mid] < target) {
-                low = mid + 1;
+            } else if (array[mid] < target) {
+                l = mid + 1;
             } else {
-                high = mid - 1;
+                r = mid - 1;
             }
         }
 
-        return low;
+        return l;
     }
 }
