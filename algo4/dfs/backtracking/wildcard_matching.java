73767de9bcee59664cffb3da6f910fab9e5ecd0c
writecoffee <writecoffee@gmail.com>
2014-04-25 02:55:06 -0400

    algo4: dfs: backtracking: wildcard_matching: Revisited

---
+++ algo4/dfs/backtracking/wildcard_matching.java
@@ -0,0 +1,46 @@
+package backtracking;
+
+public class wildcard_matching {
+    /**
+     * Test cases that need to go through:
+     * 
+     *      (1) s = "aa", p = "*"
+     *      (2) s = "hi", p = "*?"
+     */
+    public boolean isMatch(String s, String p) {
+        s = new String(s + '\0');
+        p = new String(p + '\0');
+
+        int i = 0;
+        int j = 0;
+        int iBack = -1;
+        int jBack = -1;
+
+        while (s.charAt(i) != '\0') {
+            if (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i)) {
+                i++;
+                j++;
+            } else if (p.charAt(j) == '*') {
+                while (p.charAt(j) == '*') {
+                    j++;
+                }
+
+                iBack = i;
+                jBack = j;
+            } else {
+                if (iBack == -1) {
+                    return false;
+                }
+
+                i = ++iBack;
+                j = jBack;
+            }
+        }
+
+        while (p.charAt(j) == '*') {
+            ++j;
+        }
+
+        return s.charAt(i) == '\0' && p.charAt(j) == '\0';
+    }
+}
