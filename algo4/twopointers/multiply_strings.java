e9f04000340c89c6615f7d07e2facd77fd4701a2
writecoffee <writecoffee@gmail.com>
2014-04-25 03:31:07 -0400

    algo4: twopointers: multiply_strings: Revisited

--- algo4/twopointers/multiply_strings.java
+++ algo4/twopointers/multiply_strings.java
@@ -1,33 +1,43 @@
+import java.util.Arrays;
+
 public class multiply_strings {
+    public String multiply(String num1, String num2) {
+        int m = num1.length();
+        int n = num2.length();
 
-    public static String multiply(String num1, String num2) {
-        int n = num1.length();
-        int m = num2.length();
+        int[] result = new int[m + n + 1];
+        int k = result.length;
 
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < n + m; i++) {
-            result.append('0');
-        }
-
-        for (int i = n - 1; i >= 0; --i) {
+        for (int i = 0, offset = 0; i < n; ++i, ++offset) {
             int carry = 0;
-            for (int j = m - 1; j >= 0; --j) {
-                int sum = carry + result.charAt(i + j + 1) - '0' + (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-                result.setCharAt(i + j + 1, (char) (sum % 10 + '0'));
-                carry = sum / 10;
+            int j = 0;
+
+            for (; j < m; ++j) {
+                int a = num1.charAt(m - 1 - j) - '0';
+                int b = num2.charAt(n - 1 - i) - '0';
+
+                int product = a * b + carry + result[k - 1 - j - offset];
+
+                result[k - 1 - j - offset] = (product % 10);
+                carry = product / 10;
             }
 
-            result.setCharAt(i, (char) (result.charAt(i) + carry));
+            result[k - 1 - j - offset] += carry;
         }
 
-        while (result.length() > 1 && result.charAt(0) == '0') {
-            result.delete(0, 1);
+        int i = 0;
+        while (i < k - 1 && result[i] == 0) {
+            ++i;
         }
 
-        return result.toString();
+        return toString(Arrays.copyOfRange(result, i, result.length));
     }
 
-    public static void main(String[] args) {
-        System.out.println(multiply("99", "977"));
+    private String toString(int[] result) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < result.length; ++i) {
+            sb.append(result[i]);
+        }
+        return sb.toString();
     }
 }
