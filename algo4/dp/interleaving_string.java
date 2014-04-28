dfe0e305bf45684f47817495049771447d8d00fc
writecoffee <writecoffee@gmail.com>
2014-04-27 21:00:50 -0400

    algo4: dp: interleaving_string: Revisited and optimized

--- algo4/dp/interleaving_string.java
+++ algo4/dp/interleaving_string.java
@@ -28,18 +28,31 @@ public class interleaving_string {
         return dp[m][n];
     }
 
-    public static void printTable(boolean[][] dp, int m, int n) {
-        for (int i = 0; i < m + 1; ++i) {
-            for (int j = 0; j < n + 1; ++j) {
-                System.out.print((dp[i][j] ? 'Y' : 'N') + " ");
-            }
+    public boolean isInterleaveOptimized(String s1, String s2, String s3) {
+        int m = s1.length();
+        int n = s2.length();
 
-            System.out.println();
+        if (m + n != s3.length()) {
+            return false;
         }
-    }
 
-    public static void main(String[] args) {
-        assert isInterleave("aabcc", "dbbca", "aadbbcbcac");
-        assert isInterleave("", "", "");
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for (int j = 1; j <= n; ++j) {
+            dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        }
+
+        for (int i = 1; i <= m; ++i) {
+            dp[0] = dp[0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+
+            for (int j = 1; j <= n; ++j) {
+                dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) ||
+                        (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+                
+            }
+        }
+
+        return dp[n];
     }
 }
