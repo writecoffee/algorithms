ba19b646023e100a64154d7b7ace6d191788dad8
writecoffee <writecoffee@gmail.com>
2014-04-29 19:01:38 -0400

    algo4: array: matrix: set_matrix_zeros: Revisited

--- algo4/array/matrix/set_matrix_zeros.java
+++ algo4/array/matrix/set_matrix_zeros.java
@@ -1,90 +1,97 @@
+package matrix;
 
 public class set_matrix_zeros {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-    public static void setZeroes(int[][] matrix) {
-        int M = matrix[0].length;
-        int N = matrix.length;
+        boolean[] rowBlock = new boolean[m];
+        boolean[] colBlock = new boolean[n];
 
-        int[] rowRecord = new int[N];
-        int[] colRecord = new int[M];
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
                 if (matrix[i][j] == 0) {
-                    rowRecord[i] = 1;
-                    colRecord[i] = 1;
+                    rowBlock[i] = true;
+                    colBlock[j] = true;
                 }
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (rowRecord[i] == 1) {
+        for (int i = 0; i < m; ++i) {
+            if (rowBlock[i]) {
+                for (int j = 0; j < n; ++j) {
                     matrix[i][j] = 0;
-                } else if (colRecord[j] == 1) {
-                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (colBlock[i]) {
+                for (int j = 0; j < m; ++j) {
+                    matrix[j][i] = 0;
                 }
             }
         }
     }
-    
-    public static void setZeroesImprove(int[][] matrix) {
-        boolean isZeroableFirstRow = false;
-        boolean isZeroableFirstCol = false;
 
-        int rows = matrix.length;
-        int cols = matrix[0].length;
+    public void setZeroesOptimized(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        for (int i = 0; i < cols; i++) {
-            if (matrix[0][i] == 0) {
-                isZeroableFirstRow = true;
-                break;
-            }
-        }
+        boolean fc = detectFirstCol(matrix, m, n);
+        boolean fr = detectFirstRow(matrix, m, n);
 
-        for (int i = 0; i < rows; i++) {
-            if (matrix[i][0] == 0) {
-                isZeroableFirstCol = true;
-                break;
-            }
-        }
-
-        for (int i = 1; i < rows; i++)
-            for (int j = 1; j < cols; j++) {
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
                 if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
                     matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
+        }
 
-        for (int j = 1; j < cols; j++) {
-            if (matrix[0][j] == 0) {
-                for (int i = 1; i < rows; i++)
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
                     matrix[i][j] = 0;
+                }
             }
         }
 
-        for (int i = 1; i < rows; i++) {
-            if (matrix[i][0] == 0) {
-                for (int j = 1; j < cols; j++)
-                    matrix[i][j] = 0;
-            }
-        }
-
-        if (isZeroableFirstRow) {
-            for (int i = 0; i < cols; i++)
-                matrix[0][i] = 0;
-        }
-
-        if (isZeroableFirstCol) {
-            for (int i = 0; i < rows; i++)
+        if (fc) {
+            for (int i = 0; i < m; ++i) {
                 matrix[i][0] = 0;
+            }
+        }
+
+        if (fr) {
+            for (int i = 0; i < n; ++i) {
+                matrix[0][i] = 0;
+            }
         }
     }
-    
-    public static int[][] test = {{1, 0}};
-    
-    public static void main(String[] args) {
-        setZeroes(test);
+
+    private boolean detectFirstRow(int[][] matrix, int m, int n) {
+        boolean f = false;
+
+        for (int i = 0; i < n; ++i) {
+            if (matrix[0][i] == 0) {
+                f = true;
+            }
+        }
+
+        return f;
+    }
+
+    private boolean detectFirstCol(int[][] matrix, int m, int n) {
+        boolean f = false;
+
+        for (int i = 0; i < m; ++i) {
+            if (matrix[i][0] == 0) {
+                f = true;
+            }
+        }
+
+        return f;
     }
 }
