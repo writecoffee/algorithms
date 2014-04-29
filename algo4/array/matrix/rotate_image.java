0150aaa30a60f1d4b96b99bf0956777369f2a845
writecoffee <writecoffee@gmail.com>
2014-04-29 19:50:28 -0400

    algo4: array: matrix: rotate_image: Revisited

--- algo4/array/matrix/rotate_image.java
+++ algo4/array/matrix/rotate_image.java
@@ -1,16 +1,17 @@
+package matrix;
+
 public class rotate_image {
     public void rotate(int[][] matrix) {
-        int depth = matrix.length / 2;
+        int n = matrix.length;
+        int d = n / 2;
 
-        for (int i = 0; i < depth; i++) {
-            int width = matrix.length - 1 - i;
-
-            for (int j = i; j < width; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[matrix.length - 1 - j][i];
-                matrix[matrix.length - 1 - j][i] = matrix[matrix.length - 1 - i][matrix.length - 1 - j];
-                matrix[matrix.length - 1 - i][matrix.length - 1 - j] = matrix[j][matrix.length - 1 - i];
-                matrix[j][matrix.length - 1 - i] = temp;
+        for (int i = 0; i < d; ++i) {
+            for (int j = i; j < n - i - 1; ++j) {
+                int t = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = t;
             }
         }
     }
