b0a58dc9b954c8be439ca0a9d4fed51453ddc8da
writecoffee <writecoffee@gmail.com>
2014-04-29 23:58:37 -0400

    algo4: array: matrix: spiral_matrix: Revisited

--- algo4/array/matrix/spiral_matrix.java
+++ algo4/array/matrix/spiral_matrix.java
@@ -1,51 +1,52 @@
+package matrix;
+
 import java.util.ArrayList;
 
 public class spiral_matrix {
     public ArrayList<Integer> spiralOrder(int[][] matrix) {
         ArrayList<Integer> result = new ArrayList<Integer>();
 
-        if (matrix.length == 0) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
             return result;
         }
 
-        int i = 0;
-        while (result.size() < matrix.length * matrix[0].length) {
-            int width = matrix[0].length - 1 - i;
-            int height = matrix.length - 1 - i;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int imin = 0, imax = m - 1;
+        int jmin = 0, jmax = n - 1;
 
-            if (i == height) {
-                for (int j = i; j < width + 1; ++j) {
-                    result.add(matrix[i][j]);
-                }
+        while (true) {
+            for (int j = jmin; j <= jmax; ++j) {
+                result.add(matrix[imin][j]);
+            }
 
+            if (++imin > imax) {
                 break;
             }
 
-            if (i == width) {
-                for (int j = i; j < height + 1; ++j) {
-                    result.add(matrix[j][i]);
-                }
+            for (int i = imin; i <= imax; ++i) {
+                result.add(matrix[i][jmax]);
+            }
 
+            if (jmin > --jmax) {
                 break;
             }
 
-            for (int j = i; j < width; j++) {
-                result.add(matrix[i][j]);
+            for (int j = jmax; j >= jmin; --j) {
+                result.add(matrix[imax][j]);
             }
 
-            for (int j = i; j < height; j++) {
-                result.add(matrix[j][matrix[0].length - 1 - i]);
+            if (imin > --imax) {
+                break;
             }
 
-            for (int j = i; j < width; j++) {
-                result.add(matrix[matrix.length - 1 - i][matrix[0].length - 1 - j]);
+            for (int i = imax; i >= imin; --i) {
+                result.add(matrix[i][jmin]);
             }
 
-            for (int j = i; j < height; j++) {
-                result.add(matrix[matrix.length - 1 - j][i]);
+            if (++jmin > jmax) {
+                break;
             }
-
-            i++;
         }
 
         return result;
