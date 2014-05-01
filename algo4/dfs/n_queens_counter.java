3b994245ca7092d1ded88f0b1db54998329dba2e
writecoffee <writecoffee@gmail.com>
2014-04-30 22:43:54 -0400

    algo4: dfs: n_queens_counter: Revisited

--- algo4/dfs/n_queens_counter.java
+++ algo4/dfs/n_queens_counter.java
@@ -1,19 +1,21 @@
 public class n_queens_counter {
     public int totalNQueens(int n) {
-        return probe(n, 0, 0, 0);
+        return explore(n, 0, 0, 0, 0);
     }
 
-    private int probe(int n, int bitsCols, int bitsLds, int bitsRds) {
-        if (bitsCols == (1 << n) - 1) {
+    private int explore(int n, int i, int colMask, int ldMask, int rdMask) {
+        if (i == n) {
             return 1;
         }
 
         int result = 0;
-        int bitsAvailable = ~(bitsCols | bitsLds | bitsRds);
-        for (int i = 0; i < n; i++) {
-            int bCol = 1 << i;
-            if ((bCol & bitsAvailable) != 0) {
-                result += probe(n, bitsCols | bCol, (bitsLds | bCol) << 1, (bitsRds | bCol) >> 1);
+        int mask = colMask | ldMask | rdMask;
+
+        for (int j = 0; j < n; ++j) {
+            int p = 1 << j;
+
+            if ((p & mask) == 0) {
+                result += explore(n, i + 1, p | colMask, (p | ldMask) << 1, (p | rdMask) >> 1);
             }
         }
 
