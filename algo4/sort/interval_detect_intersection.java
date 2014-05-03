04df2561a67e62c0c5950745729a6f512a0971ae
writecoffee <writecoffee@gmail.com>
2014-05-02 22:24:51 -0400

    algo4: sort: interval_detect_insertion: Revisited

---
+++ algo4/sort/interval_detect_intersection.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class interval_detect_intersection {
+    private class Interval {
+        int start;
+        int end;
+    }
+
+    private class MyInterval extends Interval {
+        public final int index;
+
+        MyInterval(int _start, int _end, final int _index) {
+            start = _start;
+            end = _end;
+            index = _index;
+        }
+    }
+
+    public void intersected(Interval[] intervals, boolean[] isIntersected) {
+        int n = intervals.length;
+
+        MyInterval[] myIntervals = new MyInterval[n];
+        for (int i = 0; i < n; ++i) {
+            myIntervals[i] = new MyInterval(intervals[i].start, intervals[i].end, i);
+        }
+
+        Arrays.sort(myIntervals, new Comparator<MyInterval>() {
+            @Override
+            public int compare(MyInterval a, MyInterval b) {
+                return a.start - b.start;
+            }
+        });
+
+        for (int i = 0; i < n - 1; ++i) {
+            MyInterval a = myIntervals[i], b = myIntervals[i + 1];
+
+            if (a.start == b.start) {
+                isIntersected[a.index] = true;
+                isIntersected[b.index] = true;
+                b.end = Math.max(a.end, b.end);
+            } else if (a.end >= b.start) {
+                isIntersected[a.index] = true;
+                isIntersected[b.index] = true;
+                b.end = Math.max(a.end, b.end);
+            }
+        }
+    }
+}
