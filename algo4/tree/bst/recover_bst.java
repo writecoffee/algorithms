a825d725d4f529e53e447b3efa07d11817e08569
writecoffee <writecoffee@gmail.com>
2014-05-07 23:59:44 -0400

    algo4: tree: bst: recover_bst: Revisited

--- algo4/tree/bst/recover_bst.java
+++ algo4/tree/bst/recover_bst.java
@@ -3,7 +3,7 @@ package bst;
 import java.util.Stack;
 
 public class recover_bst {
-    public static class TreeNode {
+    public class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -13,37 +13,37 @@ public class recover_bst {
         }
     }
 
-    public static void recoverTree(TreeNode root) {
+    public void recoverTree(TreeNode root) {
         if (root == null || (root.left == null && root.right == null)) {
             return;
         }
 
         TreeNode[] t = new TreeNode[2];
-        inorderExplore(root, t, null);
+        explore(root, t, null);
         swap(t[0], t[1]);
     }
 
-    private static TreeNode inorderExplore(TreeNode root, TreeNode[] t, TreeNode pre) {
+    private TreeNode explore(TreeNode root, TreeNode[] t, TreeNode pre) {
         if (root == null) {
             return pre;
         }
 
-        TreeNode last = inorderExplore(root.left, t, pre);
+        TreeNode last = explore(root.left, t, pre);
         if (last != null && last.val > root.val) {
             t[1] = root;
             t[0] = t[0] == null ? last : t[0];
         }
 
-        return inorderExplore(root.right, t, root);
+        return explore(root.right, t, root);
     }
 
-    private static void swap(TreeNode l, TreeNode r) {
+    private void swap(TreeNode l, TreeNode r) {
         int temp = l.val;
         l.val = r.val;
         r.val = temp;
     }
 
-    public static void recoverTreeNonrecur(TreeNode root) {
+    public void recoverTreeNonrecur(TreeNode root) {
         if (root == null || (root.left == null && root.right == null)) {
             return;
         }
