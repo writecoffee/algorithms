a1dfc03a04fe6b417f3fd2f3b6b71c9ddbc43c77
writecoffee <writecoffee@gmail.com>
2014-05-08 22:42:15 -0400

    algo4: dp: unique_binary_search_tree: Revisited

--- algo4/dp/unique_binary_search_tree.java
+++ algo4/dp/unique_binary_search_tree.java
@@ -1,18 +1,22 @@
 public class unique_binary_search_tree {
-    static int numTrees(int n) {
+    /**
+     * For a given number n, there will be 0 to n - 1 number of node(s)
+     * in the left sub-tree. Let this number be k, and there will be
+     * only n - 1 - k number of nodes in the right sub-tree.
+     * 
+     * R(k) = SUM_i from 0 to k - 1, R(i) * R(k - 1 - i)
+     * R(0) = 1 
+     */
+    public int numTrees(int n) {
         int[] dp = new int[n + 1];
         dp[0] = 1;
 
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 0; j < i; j++) {
-                dp[i] += dp[j] * dp[i - j - 1];
+        for (int k = 1; k < n + 1; k++) {
+            for (int j = 0; j < k; j++) {
+                dp[k] += dp[j] * dp[k - j - 1];
             }
         }
 
         return dp[n];
-    }
-
-    public static void main(String[] args) {
-        System.out.println(numTrees(3));
     }
 }
