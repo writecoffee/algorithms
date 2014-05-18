3199a49b60b56c29aaebaa90c2c9b5a7c110ed8d
writecoffee <writecoffee@gmail.com>
2014-05-18 02:09:39 -0400

    algo4: dp: gmin: Revisited all

--- algo4/dp/gmin/dp_minimum_path_sum_in_grid.java
+++ algo4/dp/gmin/dp_minimum_path_sum_in_grid.java
@@ -1,44 +1,30 @@
 package gmin;
 
+/**
+ * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right
+ * which minimizes the sum of all numbers along its path.
+ * 
+ * Note: You can only move either down or right at any point in time.
+ * 
+ * http://oj.leetcode.com/problems/minimum-path-sum/
+ * 
+ */
 public class dp_minimum_path_sum_in_grid {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] table = new int[m][n];
-
-        table[0][0] = grid[0][0];
-        for (int i = 1; i < n; i++) {
-            table[0][i] = grid[0][i] + table[0][i - 1];
-        }
-
-        for (int j = 1; j < m; j++) {
-            table[j][0] = grid[j][0] + table[j - 1][0];
-        }
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                table[i][j] = Math.min(table[i - 1][j], table[i][j - 1]) + grid[i][j];
-            }
-        }
-
-        return table[m - 1][n - 1];
-    }
-
-    public int minPathSumOptimized(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
         int[] dp = new int[n];
-
         dp[0] = grid[0][0];
-        for (int i = 1; i < n; i++) {
-            dp[i] = grid[0][i] + dp[i - 1];
+
+        for (int j = 1; j < n; ++j) {
+            dp[j] = grid[0][j] + dp[j - 1];
         }
 
-        for (int i = 1; i < m; i++) {
-            dp[0] += grid[i][0];
+        for (int i = 1; i < m; ++i) {
+            dp[0] = grid[i][0] + dp[0];
 
-            for (int j = 1; j < n; j++) {
-                dp[j] = Math.min(dp[j - 1], dp[j]) + grid[i][j];
+            for (int j = 1; j < n; ++j) {
+                dp[j] = grid[i][j] + Math.min(dp[j], dp[j - 1]);
             }
         }
 
--- algo4/dp/gmin/dp_minimum_path_sum_in_triangle.java
+++ algo4/dp/gmin/dp_minimum_path_sum_in_triangle.java
@@ -2,6 +2,23 @@ package gmin;
 
 import java.util.ArrayList;
 
+/**
+ * Given a triangle, find the minimum path sum from top to bottom. Each
+ * step you may move to adjacent numbers on the row below.
+ * 
+ * For example, given the following triangle
+ * [
+ *      [2],
+ *     [3,4],
+ *    [6,5,7],
+ *   [4,1,8,3]
+ * ]
+ * 
+ * The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+ * 
+ * http://oj.leetcode.com/problems/triangle/
+ * 
+ */
 public class dp_minimum_path_sum_in_triangle {
     public int minimumTotalInplace(ArrayList<ArrayList<Integer>> triangle) {
         for (int i = triangle.size() - 2; i >= 0; --i) {
@@ -16,19 +33,17 @@ public class dp_minimum_path_sum_in_triangle {
     }
 
     public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
-        int[] dp = new int[triangle.size()];
+        int m = triangle.size();
+        int n = triangle.get(m - 1).size();
+        int[] dp = new int[n];
 
-        if (triangle.size() == 1) {
-            return triangle.get(0).get(0);
-        }
-        
-        for (int i = 0; i < triangle.get(triangle.size() - 1).size(); i++) {
-            dp[i] = triangle.get(triangle.size() - 1).get(i);
+        for (int j = 0; j < n; ++j) {
+            dp[j] = triangle.get(m - 1).get(j);
         }
 
-        for (int i = triangle.size() - 2; i >= 0; --i) {
-            for (int j = 0; j < triangle.get(i).size(); j++) {
-                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
+        for (int i = m - 2; i >= 0; --i) {
+            for (int j = 0; j < triangle.get(i).size(); ++j) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
             }
         }
 
