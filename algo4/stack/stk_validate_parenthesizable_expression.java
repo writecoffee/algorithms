9ad3436f5bbaf3be2a425bde05ff1f17c4e47892
writecoffee <writecoffee@gmail.com>
2014-06-01 20:29:03 -0400

    algo: stack: Validate parenthesizable expression

--- algo4/stack/stk_validate_parenthesizable_expression.java
+++ algo4/stack/stk_validate_parenthesizable_expression.java
@@ -1,4 +1,3 @@
-package recursion;
 import java.util.Stack;
 
 /**
@@ -20,11 +19,30 @@ import java.util.Stack;
  * {@linkplain http://www.itint5.com/oj/41/#41}
  *
  */
-public class stk_validate_expression {
+public class stk_validate_parenthesizable_expression {
     private enum Token {
         EMPTY, NUMBER, OPERATOR, L_PAREN, R_PAREN
     }
 
+    /**
+     * To express the grammer in BNF form, we have
+     * 
+     * expr := num | ( expr ) | expr bop expr
+     * bop := + | -
+     * num := [1-9]*[0-9]
+     * 
+     * Intuitively, we can parse the input in a recursive manner, while we keep
+     * track of the previous "token" parsed.
+     * 
+     * The key observation here is that the final output should be a value. For the
+     * last token we parsed, we can convert the result by popping out all previous
+     * token stored in the stack.
+     * 
+     * So we can use a stack to keep track of this, and the expected result should be
+     * resulted from a stack with just a number token only. We can also push a dummy
+     * value into the stack in case of underflow.
+     * 
+     */
     public boolean validate(String expr) {
         int n = expr.length();
         Stack<Token> s = new Stack<Token>();
@@ -54,13 +72,13 @@ public class stk_validate_expression {
                     s.push(Token.NUMBER);
                 }
             } else {
-                i = pollNumber(expr, i);
+                int len = pollNumber(expr, i);
 
-                if (i == -1) {
+                if (len == -1) {
                     return false;
-                } else {
-                    i--;
                 }
+
+                i += len - 1;
 
                 if (s.peek() == Token.OPERATOR) {
                     s.pop();
@@ -74,18 +92,18 @@ public class stk_validate_expression {
     }
 
     private int pollNumber(String expr, int i) {
-        int j = i;
         int n = expr.length();
-        boolean isZerp = expr.charAt(i) == '0';
+        boolean isZero = expr.charAt(i) == '0';
 
-        while (j < n && expr.charAt(j) >= '0' && expr.charAt(j) <= '9') {
+        int j = i;
+        while (j < n && Character.isDigit(expr.charAt(i))) {
             j++;
         }
 
-        if (j == i || isZerp && j - i > 1) {
+        if (isZero && j - i > 1) {
             return -1;
         }
 
-        return j;
+        return j - i;
     }
 }
