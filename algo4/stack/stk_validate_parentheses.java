407fbd07276a8295aab78c8a713f6476ac0bae97
Silao Xu (Tsu) <writecoffee@gmail.com>
2014-06-02 01:21:33 -0400

    algo4: stack: validate_parentheses: Revisited

---
+++ algo4/stack/stk_validate_parentheses.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+/**
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ *
+ * The brackets must close in the correct order, "()" and "()[]{}" are all valid
+ * but "(]" and "([)]" are not.
+ *
+ * {@linkplain https://oj.leetcode.com/problems/valid-parentheses/}
+ * 
+ */
+public class stk_validate_parentheses {
+    public boolean isValid(String s) {
+        int n = s.length();
+        Stack<Character> stk = new Stack<Character>();
+
+        for (int i = 0; i < n; ++i) {
+            char c = s.charAt(i);
+
+            if (c == '(' || c == '{' || c == '[') {
+                stk.push(c);
+            } else if (stk.isEmpty() || Math.abs(stk.pop() - c) > 2) {
+                return false;
+            }
+        }
+
+        return stk.isEmpty();
+    }
+}
