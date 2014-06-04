e9101365e39c3d1510abb8c15cc1230d0364939a
writecoffee <writecoffee@gmail.com>
2014-06-03 23:54:33 -0400

    algo4: math: divide_two_integers: Revisited

--- algo4/math/divide_two_integers.java
+++ algo4/math/divide_two_integers.java
@@ -1,22 +1,23 @@
 public class divide_two_integers {
-    public static int divide(int dividend, int divisor) {
-        int negFlag = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) ? -1 : 1;
-        long result = 0;
+    public int divide(int dividend, int divisor) {
+        int sign = (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0) ? -1 : 1;
         long a = Math.abs((long) dividend);
         long b = Math.abs((long) divisor);
+        long quotient = 0;
 
         while (a >= b) {
             long div = b;
-            long quotient = 1;
+            long q = 1;
+
             while ((div << 1) <= a) {
                 div <<= 1;
-                quotient <<= 1;
+                q <<= 1;
             }
 
             a -= div;
-            result += quotient;
+            quotient += q;
         }
 
-        return (int) result * negFlag;
+        return (int) quotient * sign;
     }
 }
