c4889e1c5de0f3ac98fa1f978aed1dc513666081
writecoffee <writecoffee@gmail.com>
2014-06-04 20:32:45 -0400

    algo4: heap: Revisited some find-k problems

--- algo4/heap/find_k_largest_elements_in_an_unsorted_array.java
+++ algo4/heap/find_k_largest_elements_in_an_unsorted_array.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -9,53 +7,36 @@ public class find_k_largest_elements_in_an_unsorted_array {
      * Time: O(n log n)
      * Space: O(n)
      */
-    public static ArrayList<Integer> getMaxK(int[] array, int k) {
-        ArrayList<Integer> result = new ArrayList<Integer>();
+    public ArrayList<Integer> getMaxK(int[] array, int k) {
         int n = array.length;
-        assert n > k;
+        Queue<Integer> pq = new PriorityQueue<Integer>(n);
 
-        Queue<Integer> pq = new PriorityQueue<Integer>(n, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer a, Integer b) {
-                return b - a;
-            }
-        });
-
-        for (int i = 0; i < n; i++) {
-            pq.add(array[i]);
+        for (Integer i : array) {
+            pq.add(i);
         }
 
-        for (int i = 0; i < k; i++) {
-            result.add(pq.poll());
-        }
-
-        return result;
+        return new ArrayList<Integer>(new ArrayList<Integer>(pq).subList(Math.max(0, n - k), n));
     }
 
     /**
      * Time: O(k log k + (n - k) log k)
      * Space: O(k)
      */
-    public static ArrayList<Integer> getMaxKWithMinHeap(int[] array, int k) {
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        Queue<Integer> pq = new PriorityQueue<Integer>(k);
+    public ArrayList<Integer> getMaxKWithMinHeap(int[] array, int k) {
         int n = array.length;
-        assert n > k;
+        Queue<Integer> pq = new PriorityQueue<Integer>(k);
 
-        int i = 0;
-        for (; i < k; i++) {
+        for (int i = 0; i < Math.min(k, n); i++) {
             pq.add(array[i]);
         }
 
-        for (; i < n; i++) {
+        for (int i = k; i < n; i++) {
             if (pq.peek() < array[i]) {
                 pq.poll();
                 pq.add(array[i]);
             }
         }
 
-        result.addAll(pq);
-        Collections.reverse(result);
-        return result;
+        return new ArrayList<Integer>(pq);
     }
 }
--- algo4/heap/find_k_smallest_number_in_row_col_wised_sorted_matrix.java
+++ algo4/heap/find_k_smallest_number_in_row_col_wised_sorted_matrix.java
@@ -1,6 +1,22 @@
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
+/**
+ * Given an n x n matrix, where every row and column is sorted in non-decreasing order. Find the
+ * kth smallest element in the given 2D array.
+ * 
+ * For example, consider the following 2D array.
+ * 
+ *         10, 20, 30, 40
+ *         15, 25, 35, 45
+ *         24, 29, 37, 48
+ *         32, 33, 39, 50
+ * 
+ * The 3rd smallest element is 20 and 7th smallest element is 30
+ * 
+ * {@linkplain http://www.geeksforgeeks.org/kth-smallest-element-in-a-row-wise-and-column-wise-sorted-2d-array-set-1/}
+ * 
+ */
 public class find_k_smallest_number_in_row_col_wised_sorted_matrix {
     class GridNode {
         public final int row;
@@ -15,14 +31,29 @@ public class find_k_smallest_number_in_row_col_wised_sorted_matrix {
     }
 
     /**
-     * Add all elements from first row to the min-heap and then "dive" into the column which
-     * contains the smallest element. We need to pop k - 1 times and the result is in the 
-     * last pop-action.
+     * A key observation here is that the smallest element is on the upper-left corner. From that position,
+     * we are pretty sure that will be the right place to start searching the k smallest number.
      * 
-     * { 10, 20, 30, 40 },
-     * { 15, 25, 35, 45 },
-     * { 25, 29, 37, 48 },
-     * { 32, 33, 39, 50 }
+     * So the second smallest number could be matrix[0][1] or matrix[1][0], if matrix[1][0] is the
+     * second smallest element, and matrix[0][1] is the third smallest element, next time we will have
+     * three elements to compete for the fourth smallest number, like below:
+     * 
+     *          $ $ # . . .
+     *          $ # . . . .
+     *          # . . . . .
+     *          . . . . . .
+     * 
+     * So we would have at most n elements to compete to be chosen as the next smallest and hence we
+     * can reclaim a max-heap of size n to store the elements to be chosen. Now the algorithm is simple,
+     * we can just add all elements in the first row then dive down into first column then first or second
+     * row, so on and so forth.
+     * 
+     * Time Complexity: The above solution involves following steps.
+     * 
+     * (1) Build a min heap which takes O(n) time
+     * 
+     * (2) Heapify k times which takes O(kLogn) time. Therefore, overall time complexity is
+     *     O(n + kLogn) time.
      * 
      */
     public int search(int[][] matrix, int k) {
