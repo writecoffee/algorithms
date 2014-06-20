35a8e7223f836001e2716226530245a1f3e43dcb
writecoffee <writecoffee@gmail.com>
2014-06-20 02:49:23 -0400

    algo4: twopointers: remove_element: Revisited

--- algo4/twopointers/tp_remove_element.java
+++ algo4/twopointers/tp_remove_element.java
@@ -1,7 +1,15 @@
+/**
+ * Given an array and a value, remove all instances of that value in place and return the new length.
+ * 
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ * 
+ * [Difficulty] - Easy
+ * [Source]     - {@linkplain https://oj.leetcode.com/problems/remove-element/}
+ * 
+ */
 public class tp_remove_element {
     public int removeElement(int[] array, int elem) {
-        int n = array.length;
-        int j = 0;
+        int n = array.length, j = 0;
 
         for (int i = 0; i < n; ++i) {
             if (array[i] != elem) {
