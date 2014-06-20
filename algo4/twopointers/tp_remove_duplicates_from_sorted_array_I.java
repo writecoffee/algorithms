0de9972c3c97323eba9f247205107260c4511f36
writecoffee <writecoffee@gmail.com>
2014-06-20 02:44:41 -0400

    algo4: twopointers: Remove duplicates from sorted array I and II

--- algo4/twopointers/tp_remove_duplicates_from_sorted_array_I.java
+++ algo4/twopointers/tp_remove_duplicates_from_sorted_array_I.java
@@ -1,15 +1,26 @@
+/**
+ * Given a sorted array, remove the duplicates in place such that each element appear only
+ * once and return the new length.
+ * 
+ * Do not allocate extra space for another array, you must do this in place with constant
+ * memory.
+ * 
+ * For example,
+ * 
+ * Given input array A = [1,1,2],
+ * 
+ * Your function should return length = 2, and A is now [1,2].
+ * 
+ * [Difficulty] - Easy
+ * [Source]     - facebook interview, {@linkplain https://oj.leetcode.com/problems/remove-duplicates-from-sorted-array/}
+ *
+ */
 public class tp_remove_duplicates_from_sorted_array_I {
     public int removeDuplicates(int[] array) {
-        int n = array.length;
+        int n = array.length, j = 0;
 
-        if (n == 0) {
-            return 0;
-        }
-
-        int j = 1;
-
-        for (int i = 1; i < n; ++i) {
-            if (array[i] != array[i - 1]) {
+        for (int i = 0; i < n; ++i) {
+            if (i == 0 || array[i] != array[i - 1]) {
                 array[j++] = array[i];
             }
         }
--- algo4/twopointers/tp_remove_duplicates_from_sorted_array_II.java
+++ algo4/twopointers/tp_remove_duplicates_from_sorted_array_II.java
@@ -1,15 +1,31 @@
+/**
+ * Given a sorted array, remove the duplicates in place such that each element appear only
+ * once and return the new length.
+ * 
+ * Now duplicates are allowed at most twice.
+ * 
+ * For example,
+ * 
+ * Given sorted array A = [1,1,1,2,2,3],
+ * 
+ * Your function should return length = 5, and A is now [1,1,2,2,3].
+ * 
+ * [Difficulty] - Medium
+ * [Source]     - facebook interview, {@linkplain https://oj.leetcode.com/problems/remove-duplicates-from-sorted-array-ii/}
+ *
+ */
 public class tp_remove_duplicates_from_sorted_array_II {
+    /**
+     * There is a trap if we use array[i - 2] to determine double duplicates.
+     * 
+     * [1,1,1,2,2,3] would become [1,1,2,3] because we overwrite original space.
+     * 
+     */
     public int removeDuplicates(int[] array) {
-        int n = array.length;
+        int n = array.length, j = 0;
 
-        if (n < 3) {
-            return n;
-        }
-
-        int j = 2;
-
-        for (int i = 2; i < n; ++i) {
-            if (!(array[i] == array[j - 1] && array[i] == array[j - 2])) {
+        for (int i = 0; i < n; ++i) {
+            if (i == 0 || i == 1 || array[i] != array[j - 2]) {
                 array[j++] = array[i];
             }
         }
