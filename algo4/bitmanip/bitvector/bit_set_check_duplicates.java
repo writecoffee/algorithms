199a8e0306b35d1c5e26833893ae05ba6b6e1446
writecoffee <writecoffee@gmail.com>
2014-06-25 23:17:15 -0400

    algo4: bitmanip: bitvector: Check duplicates

--- algo4/bitmanip/bitvector/bit_set_check_duplicates.java
+++ algo4/bitmanip/bitvector/bit_set_check_duplicates.java
@@ -1,44 +1,50 @@
+package bitvector;
+
 import java.util.ArrayList;
 
 /**
- * You have an array with all the numbers from 7 to N, where N is at most 32,000. The array may have
+ * You have an array with all the numbers from 1 to N, where N is at most 32,000. The array may have
  * duplicate entries and you do not know what N is.
  * 
  * With only 4 kilobytes of memory available, how would you print all duplicate elements in the
  * array?
  * 
- * {@linkplain CC150-10.4}
+ * [Difficulty] - Medium
+ * [Source]     - {@linkplain CC150-10.4}
  * 
  */
 public class bit_set_check_duplicates {
     private class BitSet {
         private int[] bitset;
 
-        public BitSet(int size) {
+        private BitSet(int size) {
             bitset = new int[size >> 5];
         }
 
-        public boolean get(int pos) {
+        private boolean get(int pos) {
             int wordNumber = (pos >> 5);
             int bitNumber = (pos & 0x1F);
             return (bitset[wordNumber] & (1 << bitNumber)) != 0;
         }
 
-        void set(int pos) {
+        private void set(int pos) {
             int wordNumber = (pos >> 5);
             int bitNumber = (pos & 0x1F);
             bitset[wordNumber] |= 1 << bitNumber;
         }
     }
 
-    public ArrayList<Integer> checkDuplicates(int[] array, int upperBound) {
+    public ArrayList<Integer> checkDuplicates(int[] array) {
+        return checkDuplicates(array, 32000);
+    }
+
+    private ArrayList<Integer> checkDuplicates(int[] array, int upperBound) {
         ArrayList<Integer> result = new ArrayList<Integer>();
         BitSet bs = new BitSet(upperBound);
         int n = array.length;
 
         for (int i = 0; i < n; ++i) {
-            int num = array[i];
-            int num0 = num - 1;
+            int num = array[i], num0 = num - 1;
 
             if (bs.get(num0)) {
                 result.add(num);
