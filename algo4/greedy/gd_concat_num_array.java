3d78f34e97170f21d63292db95c161b38eadc86e
writecoffee <writecoffee@gmail.com>
2014-06-26 22:55:46 -0400

    algo4: greedy: concat_num_array: Revisited

--- algo4/greedy/gd_concat_num_array.java
+++ algo4/greedy/gd_concat_num_array.java
@@ -1,13 +1,21 @@
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class concat_num_array {
-    /**
-     * For ["54", "546", "548", "60"]
-     * 
-     * The largest number is "6054854654" after concatenation.
-     * 
-     */
+/**
+ * There are n non-negative numbers in the input array, concatenate these numbers
+ * to form a largest integer.
+ *
+ * For example:
+ * 
+ * ["54", "546", "548", "60"]
+ * 
+ * The largest number is "6054854654" after concatenation.
+ * 
+ * [Difficulty] - Medium
+ * [Source]     - {@linkplain http://www.itint5.com/oj/#45}
+ * 
+ */
+public class gd_concat_num_array {
     public String biggestNum(String[] nums) {
         Arrays.sort(nums, new Comparator<String>() {
             @Override
