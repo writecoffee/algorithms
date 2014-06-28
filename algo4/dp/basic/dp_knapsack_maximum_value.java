79c5ed6244946ecef0343699e328c3cb3966105f
writecoffee <writecoffee@gmail.com>
2014-06-27 22:31:17 -0400

    algo4: dp: basic: knapsack_maximum_value: Revisited

--- algo4/dp/basic/dp_knapsack_maximum_value.java
+++ algo4/dp/basic/dp_knapsack_maximum_value.java
@@ -1,24 +1,29 @@
 package basic;
 
+/**
+ * We have n objects and a knapsack. The i-th object has positive weight w[i] and positive value
+ * v[i]. The knapsack capacity is C. We wish to select a set of objects to put in the knapsack so
+ * that the total values is maximum and without breaking the knapsack.
+ * 
+ * [Difficulty] - Medium
+ * [Source]     - Classical problem
+ * 
+ */
 public class dp_knapsack_maximum_value {
-    public int getMax(int capacity, int[] weights, int[] values) {
-        int m = weights.length;
-        int[][] dp = new int[m + 1][capacity + 1];
+    public int getMax(int capacity, int[] w, int[] v) {
+        int n = w.length;
+        int[][] dp = new int[capacity][n + 1];
 
-        for (int i = 0; i <= capacity; ++i) {
-            dp[0][i] = 0;
-        }
-
-        for (int i = 1; i <= m; ++i) {
-            for (int w = 1; w <= capacity; ++w) {
-                if (w - weights[i - 1] >= 0) {
-                    dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1]);
+        for (int i = 0; i < capacity; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (i - w[j - 1] < 0) {
+                    dp[i][j] = dp[i][j - 1];
                 } else {
-                    dp[i][w] = dp[i - 1][w];
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - w[j - 1]][j] + v[j - 1]);
                 }
             }
         }
 
-        return dp[m][capacity];
+        return dp[capacity - 1][n];
     }
 }
