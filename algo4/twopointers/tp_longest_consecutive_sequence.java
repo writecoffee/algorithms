373dad963d6e2cc7c7023f0d9116a48cf368bb2e
writecoffee <writecoffee@gmail.com>
2014-07-06 22:42:26 -0400

    algo4: twopointers: longest_consecutive_sequence: Revisited

--- algo4/twopointers/tp_longest_consecutive_sequence.java
+++ algo4/twopointers/tp_longest_consecutive_sequence.java
@@ -1,29 +1,41 @@
 import java.util.HashSet;
 
+/**
+ * Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+ * 
+ * For example,
+ * 
+ * Given [100, 4, 200, 1, 3, 2],
+ * 
+ * The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
+ * 
+ * Your algorithm should run in O(n) complexity.
+ * 
+ * [Difficulty] - Easy
+ * [Source]     - {@linkplain https://oj.leetcode.com/problems/longest-consecutive-sequence/}
+ * 
+ */
 public class tp_longest_consecutive_sequence {
-    public int longestConsecutive(int[] num) {
+    public int longestConsecutive(int[] nums) {
+        int gMax = 0;
         HashSet<Integer> h = new HashSet<Integer>();
-        int n = num.length;
-        int gMax = 1;
 
-        for (int i = 0; i < n; ++i) {
-            h.add(num[i]);
+        for (int num : nums) {
+            h.add(num);
         }
 
-        for (int i = 0; i < n; ++i) {
-            if (h.contains(num[i])) {
-                int l = num[i] - 1, r = num[i] + 1;
+        for (int num : nums) {
+            int l = num - 1, r = num + 1;
 
-                while (h.contains(l)) {
-                    h.remove(l--);
-                }
-
-                while (h.contains(r)) {
-                    h.remove(r++);
-                }
-
-                gMax = Math.max(gMax, r - l - 1);
+            while (h.contains(l)) {
+                h.remove(l--);
             }
+
+            while (h.contains(r)) {
+                h.remove(r++);
+            }
+
+            gMax = Math.max(gMax, r - l - 1);
         }
 
         return gMax;
