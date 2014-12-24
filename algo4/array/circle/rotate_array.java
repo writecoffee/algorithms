706301ce96c109fd76b0b9974e965e04c3c78d10
writecoffee <writecoffee@gmail.com>
2014-12-23 22:54:41 -0800

    algo4: array: circle: rotate_array: Polished

--- algo4/array/circle/rotate_array.java
+++ algo4/array/circle/rotate_array.java
@@ -1,11 +1,34 @@
 package circle;
 
+/**
+ * Write a function rotate(array[], d, n) that rotates array[] of size n by d elements.
+ *
+ *  +---------------------------------+
+ *  |  1  2  3  4  5  6  7  8  9  10  |
+ *  +---------------------------------+
+ *
+ * Rotation of the above array by 2 will make array
+ *
+ *  +---------------------------------+
+ *  |  9  10  1  2  3  4  5  6  7  8  |
+ *  +---------------------------------+
+ *
+ * [Difficulty] - Medium
+ * [Source]     - {@linkplain http://www.geeksforgeeks.org/array-rotation/}
+ *
+ */
 public class rotate_array {
     /**
-     * Notice that if the n and k has non-1 common divisor, if would juggle
-     * back to the starting position for one pass. So we should find the 
-     * greatest common divisor for n and k.
-     * 
+     * <p> This most cumbersome way to do it is to rotate the array k times
+     * and each round shift the element by one position. However that
+     * requires rotating each element k times and O(n * k) time complexity. </p>
+     *
+     * <p> So how about just to rotate each element to its destination slot
+     * with one stride. However, there would be contradictory example (n = 12, k = 8)
+     * which will lead to rotation mess-up because we are treating the whole array
+     * as one group/region. So the walk-around is to partition the array based on GCD
+     * of n and k and thus we can make sure it will not introduce overlapping. </p>
+     *
      */
     public void rotateOnepass(int[] array, int k) {
         int n = array.length;
@@ -16,7 +39,7 @@ public class rotate_array {
 
         k = k % n;
 
-        for (int i = 0; i < gcd(array.length, k); i++) {
+        for (int i = 0; i < gcd(n, k); i++) {
             int hold = array[(i + k) % n];
             array[(i + k) % n] = array[i];
 
